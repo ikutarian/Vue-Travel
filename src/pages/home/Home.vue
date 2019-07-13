b5ff4c274b52b693b933c9169fd1a7666c91a30c
ikutarian <saitou0217@outlook.com>
2019-07-13 17:55:17 +0800

    1. 引入reset.css 2. 引入border.css 3. 引入fastClick 4. 新增一个组件Home.vue

---
+++ src/pages/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    Hello World
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home' // 组件的名字
+}
+</script>
+
+<style>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/pages/home/Home' // 引入组件，其中 @ 表示 src/ 文件夹
 
 Vue.use(Router)
 
 export default new Router({
+  // 路由的配置
   routes: [
     {
+      // 访问根路径的时候，展示这个组件
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
