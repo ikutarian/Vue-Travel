7845a9c910fa0b55fa44062246cda94390ede1ea
ikutarian <saitou0217@outlook.com>
2019-07-14 13:55:56 +0800

    1. 引入轮播插件 vue-awesome-swiper 2. 实现 Home 组件中的轮播组件 Swiper

--- src/main.js
+++ src/main.js
@@ -3,13 +3,16 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueAwesomeSwiper from 'vue-awesome-swiper' // 引入 VueAwesomeSwiper
 import fastClick from 'fastClick' // 引入 fastClick
 import 'styles/reset.css' // 引入 reset.css
 import 'styles/border.css' // 引入 border.css
 import 'styles/iconfont.css' // 引入 iconfont.css
+import 'swiper/dist/css/swiper.css' // 引入 swiper.css
 
 Vue.config.productionTip = false
 fastClick.attach(document.body) // 使用 fastClick 库
+Vue.use(VueAwesomeSwiper) // 使用 VueAwesomeSwiper 库
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <home-header></home-header> <!-- 使用组件 -->
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from './components/Header' // 将Home作为组件容器，然后引入子组件
+import HomeSwiper from './components/Swiper'
 
 export default {
   name: 'Home', // 组件的名字
   components: { // 声明子组件
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
