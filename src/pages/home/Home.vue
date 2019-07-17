c76efbe56a20166a7b98138dd9bc5edee39c96ca
ikutarian <saitou0217@outlook.com>
2019-07-17 21:22:40 +0800

    首页组件（Home）增加热销推荐子组件（HomeRecommend）

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,6 +3,7 @@
     <home-header></home-header> <!-- 使用组件 -->
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
   </div>
 </template>
 
@@ -10,13 +11,15 @@
 import HomeHeader from './components/Header' // 将Home作为组件容器，然后引入子组件
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
 
 export default {
   name: 'Home', // 组件的名字
   components: { // 声明子组件
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend
   }
 }
 </script>
