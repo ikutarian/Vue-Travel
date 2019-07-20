1bc27df26ee290349203ad47401d59cfdbc87fbf
ikutarian <saitou0217@outlook.com>
2019-07-20 11:34:02 +0800

    优化vuex中city的赋值操作，进行异常捕获

--- src/store/index.js
+++ src/store/index.js
@@ -3,16 +3,25 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let defaultCity = '北京'
+try { // 如果localStorage无法使用的话，需要捕获异常
+  if (localStorage.city) {
+    defaultCity = localStorage.city
+  }
+} catch (e) {}
+
 export default new Vuex.Store({
   state: {
-    city: localStorage.city || '北京'
+    city: defaultCity
   },
   mutations: {
     changeCity (state, city) {
       // 改变state中的city的值
       state.city = city
       // 同时改变localStorage中的值
-      localStorage.city = city
+      try { // 如果localStorage无法使用的话，需要捕获异常
+        localStorage.city = city
+      } catch (e) {}
     }
   }
 })
