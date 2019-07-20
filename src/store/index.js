fd3ea93c6e3a81d2800517cbf7e899ef852f83bb
ikutarian <saitou0217@outlook.com>
2019-07-20 11:30:03 +0800

    更改vuex中配置的city的同时，也更新localStorage存储的city的值

--- src/store/index.js
+++ src/store/index.js
@@ -5,12 +5,14 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    city: '北京'
+    city: localStorage.city || '北京'
   },
   mutations: {
     changeCity (state, city) {
       // 改变state中的city的值
       state.city = city
+      // 同时改变localStorage中的值
+      localStorage.city = city
     }
   }
 })
